305395dd637596cb8afe28352f5a1c6962e2000c
awfulbarbie <119601129+awfulbarbie@users.noreply.github.com>
2022-12-02 05:11:07 +0300

    Add files via upload

---
+++ lab1.1yp/lab1.1yp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1._1yp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a;
+            
+            try
+            {
+                a = Convert.ToDouble(textBox1.Text);
+                if (a <= 0)
+                {
+                   MessageBox.Show("Сторона треугольника не может иметь отрицательное или нулевое значение!");
+                }
+                else
+                {
+                    textBox2.Text = ("Радиус описанной окружности равен: " + (Math.Round(a / Math.Sqrt(3),3)));
+                   
+                }
+            }
+            catch
+            { MessageBox.Show("Ошибка. Данные введены некорректно!"); }
+
+        }
+    }
+}
---
+++ lab1.2 yp/lab1.2 yp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1._2_yp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+
+
+            try
+            {
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox3.Text);
+                c = Convert.ToDouble(textBox4.Text);
+
+                if ((Math.Pow(a, 2) + (Math.Pow(b, 2)) == Math.Pow(c, 2)))
+                {
+                    textBox2.Text = ("Треугольник являтся прямоугольным");
+                }
+                else
+                {
+                    textBox2.Text = ("Треугольник не является прямоугольным");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка! Введен недопустимый символ.");
+            }
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
